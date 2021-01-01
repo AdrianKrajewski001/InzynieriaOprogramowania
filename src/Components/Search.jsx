742319ba1ead3183eaf7e677c75a498a76b22e3c
zagozdaM <74051416+zagozdaM@users.noreply.github.com>
2021-01-01 15:52:14 +0100

    search
    search

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,42 +1,108 @@
 import React, { useContext, useState } from "react";
 import { Link } from "@reach/router";
 import { auth, signInWithGoogle, generateUserDocument } from "../firebase";
-import {database} from "../firebase";
+import {firestore} from "../firebase";
+import testowaFunkcja from "../App.js"
 const Search = () => {
   
   
-var Objawy=[]
-const [Objaw, ShowValues] = useState(Objawy);
-const downloadFromDatabase= ()=>
+var Specialization=[]
+var Doctor=[];
+const [Doctors,SetDoctors]=useState(Doctor);
+const [filteredUsers, setUsers] = React.useState(Specialization);
+
+
+const downloadSpecializationsFromDatabase=async()=>
 {
-  Objawy=[];
-  var searchedValue=document.getElementById("textbox").value;
-  var ref=database.ref("Objawy");
-  console.log(searchedValue);
-  try{ref.orderByChild("Objaw").equalTo(searchedValue).on("child_added",
-   async(snapshot)=> {
-    await Objawy.push(snapshot.child("lekarz").val()) 
-    ShowValues(Objawy) 
-  } )
+  Specialization=[];
+  const citiesRef = firestore.collection('Lekarze');
+const snapshot = await citiesRef.where('Objawy', 'array-contains', document.getElementById("textbox").value).get();
+if (snapshot.empty) {
+  console.log('No matching documents.');
+  return;
+}  
+
+snapshot.forEach(async doc => {
+  console.log(doc.id, '=>', doc.data());
+  await Specialization.push(doc.id);
+  
+  downloadDoctorsfromDatabase();
+});
+
+
+
+
+
 }
-catch{console.log("error connecting to database!")}}
+
+const listItems = Specialization.map((number) =>
+    <li key={number.toString()}>      {number}
+    </li>
+
+)
+
+const downloadDoctorsfromDatabase=async()=>
+{
+
+
+const citiesRef = firestore.collection('users');
+const snapshot = await citiesRef.where('specjalizacja', 'array-contains', 'kardiolog').get();
+if (snapshot.empty) {
+  console.log('No matching documents.');
+  return;
+}  
+
+snapshot.forEach(async doc => {
+  console.log(doc.id, '=>', doc.data());
+   var x=doc.data()
+  
+  await Doctor.push(x.displayName);
+  
+});
+ setUsers(Specialization);
+ SetDoctors(Doctor)
+}
+
+
+
+
+
+
+
+
+function UsersList({ users }) {
+  if (users.length > 0) {
+    return (
+      <ul>
+        {users.map(user => <li key={user}>{user}</li>)}
+      </ul>
+    );
+  }
+
+  return (
+    <p>Brak danych</p>
+  );
+};
+
+
+
+
 
 return(
 <div><input className="border border-blue-400" id="textbox"/>
-<button className="border border-blue-500" onClick={()=>downloadFromDatabase()}>Szukaj</button>
-<List users={Objaw}/>
+
+
+<button className="border border-blue-500" onClick={()=>downloadSpecializationsFromDatabase()}>Szukaj</button>
+
+
+
+<UsersList users={filteredUsers} />
+
+<div><b>Lista lekarzy:</b></div>
+<UsersList users={Doctors}/>
 </div>
 )
-  function List({ users }) {
- if (users.length > 0) {
-   return (
-   <ul>
-  {users.map(user => <li key={user}>{user}</li>)}
-   </ul>
-   );
-    }
-    return (
-         <p>Brak wyników</p> );};
+  
 
 }
 
